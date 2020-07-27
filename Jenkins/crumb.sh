a2412e4f852e6b8b3178b3185254a666712976b4
Nithesh shetty <nsnitheshshetty71@gmail.com>
2020-07-28 04:59:57 +0530

    script to run jenkins jobs in remote server via crumb

--- Jenkins/crumb.sh
+++ Jenkins/crumb.sh
@@ -3,8 +3,8 @@
 crumb=$(curl -u "admin:1234" -s 'http://myjenkins:8080/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,":",//crumb)')
 
 #step2:running the Jnekins job via crumb using build URL(NON parameterized)
-curl -u "admin:1234" -H "$crumb" -X POST http://myjenkins:8080/job/ENV/build?delay=0sec
+curl -u "admin:1234" -H "$crumb" -X POST "http://myjenkins:8080/job/ENV/build?delay=0sec"
 
 #step2:running the Jnekins job via crumb using build URL(parameterized)
-#curl -u "admin:1234" -H "$crumb" -X POST  http://myjenkins:8080/job/backup-to-aws/buildWithParameters?MYSQL_HOST=db_host&DATABASE_NAME=testdb&AWS_BUCKET_NAME=jenkins-mysql-backup
+#curl -u "admin:1234" -H "$crumb" -X POST  "http://myjenkins:8080/job/backup-to-aws/buildWithParameters?MYSQL_HOST=db_host&DATABASE_NAME=testdb&AWS_BUCKET_NAME=jenkins-mysql-backup"
 
