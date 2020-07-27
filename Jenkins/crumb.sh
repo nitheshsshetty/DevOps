365f9a2d3bcbbc03631947bab036e3b4b2f47a8e
Nithesh Shetty <17782014+nitheshsshetty@users.noreply.github.com>
2020-07-28 03:04:59 +0530

    Update crumb.sh
    added comments

--- Jenkins/crumb.sh
+++ Jenkins/crumb.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-
+#step1:crumb token generation
 crumb=$(curl -u "admin:1234" -s 'http://myjenkins:8080/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,":",//crumb)')
+
+#step2:running the Jnekins job via crumb using build URL(NON parameterized)
 curl -u "admin:1234" -H "$crumb" -X POST http://myjenkins:8080/job/ENV/build?delay=0sec
-#curl -u "admin:1234" -H "$crumb" -X POST  http://jenkins.local:8080/job/backup-to-aws/buildWithParameters?MYSQL_HOST=db_host&DATABASE_NAME=testdb&AWS_BUCKET_NAME=jenkins-mysql-backup
+
+#step2:running the Jnekins job via crumb using build URL(parameterized)
+#curl -u "admin:1234" -H "$crumb" -X POST  http://myjenkins:8080/job/backup-to-aws/buildWithParameters?MYSQL_HOST=db_host&DATABASE_NAME=testdb&AWS_BUCKET_NAME=jenkins-mysql-backup
 
