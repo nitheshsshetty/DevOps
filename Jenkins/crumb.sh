c5332826bc2e19660b25fec5a00961a37ad80dfa
Nithesh shetty <nsnitheshshetty71@gmail.com>
2020-07-28 02:59:29 +0530

    crumb file to run jenkins jobs via localhost

---
+++ Jenkins/crumb.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+crumb=$(curl -u "admin:1234" -s 'http://myjenkins:8080/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,":",//crumb)')
+curl -u "admin:1234" -H "$crumb" -X POST http://myjenkins:8080/job/ENV/build?delay=0sec
+#curl -u "admin:1234" -H "$crumb" -X POST  http://jenkins.local:8080/job/backup-to-aws/buildWithParameters?MYSQL_HOST=db_host&DATABASE_NAME=testdb&AWS_BUCKET_NAME=jenkins-mysql-backup
+
